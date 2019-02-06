7cf271fa1dd0394d4ce99e0d90f3f34a7998f46c
Kovalev Stanislav <djently@yandex.ru>
2019-02-07 01:21:52 +0300

    Updates

---
+++ precache-manifest.f99ba644b69998e361903641042d920a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tooltip-challenge/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "993ee90ec81d55985a200fa8ab229070",
+    "url": "/tooltip-challenge/static/media/code_snippet.993ee90e.png"
+  },
+  {
+    "revision": "0fabd95fc163dcdb7ec2",
+    "url": "/tooltip-challenge/static/js/runtime~main.0fabd95f.js"
+  },
+  {
+    "revision": "b91ca7461c1d710249d8",
+    "url": "/tooltip-challenge/static/js/main.b91ca746.chunk.js"
+  },
+  {
+    "revision": "45b5fcd7a0a179ca2fc9",
+    "url": "/tooltip-challenge/static/js/1.45b5fcd7.chunk.js"
+  },
+  {
+    "revision": "b91ca7461c1d710249d8",
+    "url": "/tooltip-challenge/static/css/main.e109ed3f.chunk.css"
+  },
+  {
+    "revision": "79ef1d09a85d17570b8b0d851a7481be",
+    "url": "/tooltip-challenge/index.html"
+  }
+];
