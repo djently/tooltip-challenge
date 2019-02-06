6951c2d13f3acdc49240fdb2435773373f4146aa
Kovalev Stanislav <djently@yandex.ru>
2019-02-07 00:35:57 +0300

    more text for in example

--- src/App.js
+++ src/App.js
@@ -55,6 +55,33 @@ class App extends Component {
           to get you started.
         </p>
 
+        <header>Why JSX?</header>
+        <p>
+          React embraces the fact that rendering logic is inherently coupled
+          with other UI logic: how events are handled,
+          how the state changes over time,
+          and how the data is prepared for display.
+        </p>
+
+        <p>
+          Instead of artificially separating technologies by putting
+          markup and logic in separate files,
+          React separates concerns with loosely coupled units
+          called “components” that contain both.
+          We will come back to components in a further section,
+          but if you’re not yet comfortable putting markup in JS,
+          this talk might convince you otherwise.
+        </p>
+
+        <p>
+          React doesn’t require using JSX, but most people find it helpful as
+          a visual aid when working with UI inside the JavaScript code.
+          It also allows React to show more useful error and warning messages.
+        </p>
+
+        <p>With that out of the way, let’s get started!</p>
+
+
         <p className={articleStyles.buttonContainer}>
           <Tooltip content="A tooltip for the button!" position="right">
             <button type="button">Hover me!</button>
